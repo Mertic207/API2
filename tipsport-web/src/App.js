97fb4a5544de4339b565d6c7fca69d9f2c544283
Mertic207 <159769502+Mertic207@users.noreply.github.com>
2025-06-12 19:05:42 +0000

    12.6

--- tipsport-web/src/App.js
+++ tipsport-web/src/App.js
@@ -1,24 +1,13 @@
-import MatchList from "./components/MatchList";
-
-
-function App() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Tipsport Webovka</h1>
-      <MatchList />
-    </div>
-  );
-}
-
-export default App;
-
+import React from "react";
 import Navbar from "./components/Navbar";
+import MatchList from "./components/MatchList";
 
 function App() {
   return (
     <div>
       <Navbar />
       <div style={{ padding: 20 }}>
+        <h1>Tipsport Webovka</h1>
         <MatchList />
       </div>
     </div>
